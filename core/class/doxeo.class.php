27fdce13d8cbed4df0e99398bac657f35346043a
root <root@raspberrypi>
2018-10-12 10:33:11 +0200

    first commit

---
+++ core/class/doxeo.class.php
@@ -0,0 +1,253 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+
+class doxeo extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+
+
+    /*     * ***********************Methode static*************************** */
+    
+    public static function callDoxeoMonitor($_url) {
+        $url = 'http://127.0.0.1:' . config::byKey('port_server', 'doxeo', 8080) . '/' . $_url;
+		$ch = curl_init();
+		curl_setopt_array($ch, array(
+			CURLOPT_URL => $url,
+			CURLOPT_HEADER => false,
+			CURLOPT_RETURNTRANSFER => true,
+		));
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+			$curl_error = curl_error($ch);
+			curl_close($ch);
+			throw new Exception(__('Echec de la requête http : ', __FILE__) . $url . ' Curl error : ' . $curl_error, 404);
+		}
+		curl_close($ch);
+		return (is_json($result)) ? json_decode($result, true) : $result;
+	}
+    
+    public static function deamon_info() {
+		$return = array();
+		$return['state'] = 'ok';
+        $return['launchable'] = 'ok';
+        
+        $url = 'http://127.0.0.1:' . config::byKey('port_server', 'doxeo', 8080) . '/system.js';
+		$ch = curl_init();
+		curl_setopt_array($ch, array(
+			CURLOPT_URL => $url,
+			CURLOPT_HEADER => false,
+			CURLOPT_RETURNTRANSFER => true,
+		));
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+            $return['state'] = 'nok';
+		} else if (!is_json($result)) {
+            $json = json_decode($result, true);
+            if (!$json['success']) {
+                $return['state'] = 'nok';
+            }
+        }
+		curl_close($ch);
+        
+        if ($return['state'] == 'nok') {
+            $daemonPath = config::byKey('daemon_path', 'doxeo', './doxeo');
+            
+            if (($daemonPath != './doxeo' || $daemonPath != './doxeo-monitor') 
+                    && !file_exists($daemonPath)) {
+                $return['launchable'] = 'nok';
+                $return['launchable_message'] = 'doxeo daemon is missing at ' + $daemonPath;
+            }
+        }
+		
+		return $return;
+	}
+    
+    public static function deamon_start($_debug = false) {
+        $callback = network::getNetworkAccess('internal', 'proto:127.0.0.1:port:comp') . '/plugins/doxeo/core/php/jeeDoxeo.php';
+        $daemonPath = config::byKey('daemon_path', 'doxeo', './doxeo');
+        $daemonViewPath = config::byKey('daemon_view_path', 'doxeo', '');
+
+        $cmd = $daemonPath . ' ';
+        if ($daemonViewPath != '') {
+            $cmd .= ' --path ' . $daemonViewPath;
+        }
+		$cmd .= ' --callback ' . $callback;
+		$cmd .= ' --apikey ' . jeedom::getApiKey('doxeo');
+
+        log::add('doxeo', 'info', 'Starting doxeo daemon: ' . $cmd);
+        exec($cmd . ' >> ' . log::getPathToLog('doxeo') . ' 2>&1 &');
+        
+        $i = 0;
+        while ($i < 10) {
+            $deamon_info = self::deamon_info();
+            if ($deamon_info['state'] == 'ok') {
+				break;
+            }
+			sleep(1);
+			$i++;
+		}
+		if ($i >= 10) {
+			log::add('doxeo', 'error', 'Unable to start doxeo daemon ' .$callback, 'unableStartDeamon');
+			return false;
+		}
+        
+		message::removeAll('doxeo', 'unableStartDeamon');
+		log::add('doxeo', 'info', 'Doxeo daemon started');
+        return true;
+	}
+    
+    public static function deamon_stop() {
+        doxeo::callDoxeoMonitor('stop');
+        
+        $i = 0;
+        while ($i < 10) {
+            $deamon_info = self::deamon_info();
+            if ($deamon_info['state'] == 'nok') {
+				break;
+            }
+			sleep(1);
+			$i++;
+		}
+		if ($i >= 10) {
+			log::add('doxeo', 'error', 'Unable to stop doxeo daemon ' .$callback, 'unableStopDeamon');
+			return false;
+        }
+        
+        message::removeAll('doxeo', 'unableStopDeamon');
+        log::add('doxeo', 'info', 'Daemon doxeo stopped');
+        return true;
+	}
+
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      public static function cronHourly() {
+
+      }
+     */
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDaily() {
+
+      }
+     */
+
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert() {
+        
+    }
+
+    public function postInsert() {
+        
+    }
+
+    public function preSave() {
+        
+    }
+
+    public function postSave() {
+        
+    }
+
+    public function preUpdate() {
+        
+    }
+
+    public function postUpdate() {
+        
+    }
+
+    public function preRemove() {
+        
+    }
+
+    public function postRemove() {
+        
+    }
+
+    /*
+     * Non obligatoire mais permet de modifier l'affichage du widget si vous en avez besoin
+      public function toHtml($_version = 'dashboard') {
+
+      }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action après modification de variable de configuration
+    public static function postConfig_<Variable>() {
+    }
+     */
+
+    /*
+     * Non obligatoire mais ca permet de déclencher une action avant modification de variable de configuration
+    public static function preConfig_<Variable>() {
+    }
+     */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+class doxeoCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array()) {
+        
+        if ($this->getType() != 'action') {
+			return;
+		}
+        
+        $id = $this->getEqLogic()->getLogicalId();
+        $command = $this->getConfiguration('command');
+        
+        $request = 'switch/change_switch_status?id=' . $id . '&status=' . $command;
+        log::add('doxeo', 'info', 'send request: ' . $request);
+        
+        doxeo::callDoxeoMonitor($request);
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+
---
+++ core/php/jeeDoxeo.php
@@ -0,0 +1,67 @@
+<?php
+
+/* This file is part of Jeedom.
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+
+if (!jeedom::apiAccess(init('apikey'), 'doxeo')) {
+	echo __('Vous n\'etes pas autorisé à effectuer cette action', __FILE__);
+	die();
+}
+
+if (isset($_GET['test'])) {
+	echo 'OK';
+	die();
+}
+
+$results = json_decode(file_get_contents("php://input"), true);
+if (!is_array($results)) {
+    echo 'NO JSON';
+	die();
+}
+
+if (isset($results['type'])) {
+	$eqLogic = doxeo::byLogicalId($results['id'], 'doxeo');
+	if (is_object($eqLogic)) {
+		foreach ($eqLogic->getCmd('info') as $cmd) {
+			log::add('doxeo', 'info', $results['type'] . ';' . $results['id'] . ';' . $results['value'] );
+
+			if ($cmd->getSubType() == 'binary') {
+				if ($results['value'] == "started") {
+					$results['value'] = 1;
+				}
+
+				if ($results['value'] == "close") {
+					$results['value'] = 1;
+				}
+
+				if ($results['value'] == "event") {
+					$results['value'] = 1;
+				}
+			}
+
+			$cmd->event($results['value']);
+		}
+		if ($result['CommandClass'] == '128') {
+			$eqLogic->batteryStatus($result['value']);
+		}
+	}
+}
+
+if (isset($results['message'])) {
+	log::add('doxeo', 'error', $results['message']);
+}
